8861c28242b5d8bacb380c698e8d3bc220ee2a16
cabralRodrigo <rodrigopaivac@gmai.com>
2015-06-06 03:12:56 -0300

    The comment update #1.1
    Ops... forgot this..

--- src/ts/render/GameRenderer.ts
+++ src/ts/render/GameRenderer.ts
@@ -5,11 +5,20 @@ class GameRenderer {
 	private cellRenderer : CellRenderer;
 	private ctx : CanvasRenderingContext2D;
 	
+	/**
+	 * Creates an new instace of the GameRenderer class
+	 * @param canvas The canvas to render the gamme
+	 * @param setting The settings for the current game
+	 */
 	constructor(private canvas: HTMLCanvasElement, private setting: GameSetting) {		
 		this.ctx = <CanvasRenderingContext2D>this.canvas.getContext('2d');
 		this.cellRenderer = new CellRenderer(this.ctx, setting);
 	}
 		
+	/**
+	 * Renders the game
+	 * @param field The field of the game
+	 */
 	public render(field: Field): void {
 		this.canvas.height = field.getYCellCount() * this.setting.cellHeight;
 		this.canvas.width = field.getXCellCount() * this.setting.cellWidth;
@@ -20,6 +29,12 @@ class GameRenderer {
 		field.forEachField((x, y, cell) => this.cellRenderer.renderCell(x, y, cell));
 	}
 	
+	/**
+	 * Renders the an specific cell on a field
+	 * @param field The field to render the cell
+	 * @param x The number of the cell on the X axis
+	 * @param y The number of the cell on the Y axis 
+	 */
 	public renderAt(field: Field, x: number, y: number) {
 		var cell = field.getCellAt(x, y);
 		if (cell)
