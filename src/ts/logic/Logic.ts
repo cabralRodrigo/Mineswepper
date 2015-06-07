11053a813e3bdc83cd1922b6eb705d6a89435089
cabralRodrigo <rodrigopaivac@gmail.com>
2015-06-07 13:01:15 -0300

    Removed the FieldMatrix in favor of Matrix2D #2
    Forgot this file.

--- src/ts/logic/Logic.ts
+++ src/ts/logic/Logic.ts
@@ -1,7 +1,6 @@
 import {Cell, CellState} from './Cell'
 import {Field} from './Field'
-import {FieldMatrix} from './FieldMatrix'
 import {InputHandler} from './Inputhandler'
 import {GameSettings, GameSetting} from './GameSetting'
 
-export {Cell, CellState, Field, FieldMatrix, InputHandler, GameSettings, GameSetting}
+export {Cell, CellState, Field, InputHandler, GameSettings, GameSetting}
